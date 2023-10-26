0a7c30879c6b9ef6d1f3fdba30cda737cd15d2a2
Rohit-Kalavapudi <93667768+Rohit-Kalavapudi@users.noreply.github.com>
2023-10-26 13:54:52 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ const port = process.env.PORT||4000
 app.use(express.json())
 // app.use(cors)
 app.use(cors({
-    origin: 'http://localhost:3000'
+    origin: 'https://vercel.com/rohits-projects/app-deploy-powerai'
   }));
 
  app.use('/api/auth', require('./routes/auth'))
